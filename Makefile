ee106a671e49d5103e4a41a9351e26ed89890caf
Xiaocong Zhang <xiaocongz@student.unimelb.edu.au>
2022-12-05 18:44:58 +0800

    organize tests with a simple test template tree

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
-all: main.o parse.o cJSON.o
-	cc -fsanitize=address -o screen main.o parse.o cJSON.o
+all: main.o parse.o cJSON.o main_asan.o parse_asan.o cJSON_asan.o
+	cc -o screen main.o parse.o cJSON.o
+	cc -fsanitize=address -o screen_asan main_asan.o parse_asan.o cJSON_asan.o
 
 
 main.o: main.c
-	cc -g -fsanitize=address -c  main.c -o main.o
+	cc -g -c  main.c -o main.o
+	cc -g -fsanitize=address -c  main.c -o main_asan.o
 parse.o: parse.c
-	cc -g -fsanitize=address -c parse.c -o parse.o # Runs second
+	cc -g -c parse.c -o parse.o
+	cc -g -fsanitize=address -c parse.c -o parse_asan.o
 cJSON.o: third_party/cJSON/cJSON.c
-	cc -g -fsanitize=address -c third_party/cJSON/cJSON.c -o cJSON.o
+	cc -g -c third_party/cJSON/cJSON.c -o cJSON.o
+	cc -g -fsanitize=address -c third_party/cJSON/cJSON.c -o cJSON_asan.o
 clean:
-	rm *.o screen
+	rm *.o screen screen_asan
--- parse.c
+++ parse.c
@@ -2,33 +2,63 @@
 #include <string.h>
 #include <stdio.h>
 #include "parse.h"
+
+int check_url(char *url){
+	char *p;
+	if(strchr(url, ' ') != NULL){
+		//url contains space, error
+		return -1;
+	}
+
+	p = strchr(url, '?');
+	if(p == NULL){
+		//valid url with no query string
+		return 0;
+	}else if(strchr(p+1, '?') != NULL){
+		//more than one '?', error
+		return -1;
+	}
+
+
+}
+
+
+
 /*
 only allocate mem for params
 key value are in place(in truncated part of url)
-TODO free mem
+Currently, anchors, i.e. things after "#", that are after query string are discarded. urls with no query string are unaffected.
 */
 int
 parse_query_string(char* url, struct query_parameter** out)
 {
-	/* TODO */
+	// TODO free strdup
+	char *urldup = strdup(url);
+
 	*out = NULL; //so that realloc works
 	int parameter_count = 0;
 	char *query_string, *question_mark;
 
-	question_mark = strchr(url, '?');
-
-
-	//No query_string, no '?'
-	if(question_mark == NULL){
-		printf("Has no '?'; No parameters\n");
+	int res = check_url(url);
+	if (res == -1){
+		free(urldup);
+		return parameter_count;
+	}else if (res == 0){
+		free(urldup);
 		return parameter_count;
 	}
-	else{
-		//modify url here
-		query_string = question_mark + 1;
-		*question_mark = '\0';
+
+	//if res == 1, has one '?'
+	question_mark = strchr(url, '?');
+	query_string = question_mark + 1;
+	*question_mark = '\0';
+	char *anchor = strchr(query_string, '#');
+	if (anchor != NULL){
+		*anchor = '\0';
 	}
-	//query_string is not empty
+
+
+	//while query_string is not empty
 	while(query_string[0] != '\0'){
 		char *query_k, *query_v, *equal_sign;
 		char *ep;
@@ -50,8 +80,15 @@ parse_query_string(char* url, struct query_parameter** out)
 			struct query_parameter *query = *out + parameter_count;
 			query->name = query_k;
 			query->value = query_v;
-			
 			parameter_count++;
+			//if query_v contains '=', error
+			if(strchr(query_v, '&') == NULL){
+				if(strchr(query_v, '=') != NULL){
+					goto error;
+				}
+			}else if (strchr(query_v, '=') != NULL && strchr(query_v, '=') < strchr(query_v, '&')){
+				goto error;
+			}
 		}
 		if(*ep == '&'){
 			query_string = ep + 1;
@@ -60,10 +97,15 @@ parse_query_string(char* url, struct query_parameter** out)
 			query_string = ep;
 		}
 	}
-
+	free(urldup);
 	return parameter_count;
 
 error:
-	printf("ERROR: \n");
+	//restore url, clean query parameters, free urldup
+	strcpy(url, urldup);
+	free(urldup);
+	free(*out);
+	*out = NULL;
+	parameter_count = 0;
 	return parameter_count;
 }
